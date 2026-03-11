06071ea8095f793e5f32616717f941e71b2922a2
r p <rpischenko@gmail.com>
2026-03-11 23:55:06 +0300

    Moved tests to another dir, updated input options

--- PischenkoR_131_01072025/main.c
+++ PischenkoR_131_01072025/main.c
@@ -8,6 +8,7 @@ FILE* outFile;
 int main(int argc, char ** argv[]) {
     Field startPos;
     FinalPosition position;
+    const char* oF = argc == 2 ? (const char*)argv[1] : "./input.txt";
     const char* oF = argc == 3 ? (const char*)argv[2] : "./result.txt";
     outFile = fopen(oF, "w");
     position = input((const char *)argv[1], &startPos);
